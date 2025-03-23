e79a391830974d9b1003629b4d91903678a7bae4
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2025-03-23 10:24:33 +0530

    added crud in form router

--- src/router/formRouter.js
+++ src/router/formRouter.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const formRouter = express.Router();
 const Form = require("../models/createForm.schema");
+const mongoose = require("mongoose")
 
 formRouter.post("/createForm/:userId", async(req,res) => {
     try {
         const {userId} = req.params;
         const {title,sections} = req.body;
-        console.log(title)
-        console.log(sections)
         if(!title || !sections){
             return res.status(400).json({message:"title and sections are required"})
         }
@@ -33,6 +32,24 @@ formRouter.get("/getForms/:userId", async(req,res) => {
         res.status(500).json({message:error.message})
     }
 })
+formRouter.get("/getFormsById/:formId", async (req, res) => {
+    try {
+        const { formId } = req.params;
+
+        if (!mongoose.isValidObjectId(formId)) {
+            return res.status(400).json({ message: "Invalid Form ID" });
+        }
+
+        const form = await Form.findById(formId);
+        if (!form) {
+            return res.status(404).json({ message: "Form not found." });
+        }
+
+        res.status(200).json({ forms: [form] }); 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 formRouter.delete("/deleteForm/:formId", async (req, res) => {
     try {
@@ -50,6 +67,33 @@ formRouter.delete("/deleteForm/:formId", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+formRouter.put("/updateForm/:formId", async (req,res) => {
+    try {
+        const {formId} = req.params;
+        const {title,sections} = req.body;
+        if(!title || !sections){
+            return res.status(400).json({message:"title and section are required."})
+        }
+
+        if (!mongoose.isValidObjectId(formId)) {
+            return res.status(400).json({ message: "Invalid Form ID" });
+          }
+        const form = await Form.findById(formId)
+        if(!form){
+            return res.status(404).json({message:"Form not found."})
+        }
+
+        form.title = title;
+        form.sections = sections;
+        form.markModified("sections"); 
+        await form.save();
+        res.status(200).json({message:"Form updated successfully",form})
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
 
 
 module.exports = formRouter;
